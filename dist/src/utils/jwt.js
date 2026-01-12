201d227472f87921d1f1e5598376f041f9f396b4
Farah Nasreen <farahnasreen2000@gmail.com>
2026-01-12 14:14:12 +0530

    Build

---
+++ dist/src/utils/jwt.js
@@ -0,0 +1,10 @@
+import jwt from "jsonwebtoken";
+import { env } from "../config/env.js";
+export const generateAccessToken = (payload) => {
+    return jwt.sign(payload, env.jwt.secret, {
+        expiresIn: "15m"
+    });
+};
+export const generateRefreshToken = (userId) => {
+    return jwt.sign({ id: userId }, env.jwt.refreshSecret, { expiresIn: "7d" });
+};
